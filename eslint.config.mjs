1cc7677396378ed9c72321fa28cfb4e5eae855d6
priyanshisoni <priyanshisoni7227@gmail.com>
2025-04-01 16:18:30 +0530

    resolve eslint.config error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,18 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
+// const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    parser: "@babel/eslint-parser", // Ensure Babel parser is used
+    parserOptions: {
+      ecmaVersion: 2020,  // Supports modern JavaScript features
+      sourceType: "module", // To allow import/export syntax
+      ecmaFeatures: {
+        jsx: true, // If you're using JSX with React
+      },
+    },
+  },
+];
 export default eslintConfig;
